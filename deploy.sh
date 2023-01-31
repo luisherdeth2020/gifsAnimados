ec060dea4fbb5adc090c136ee910b16db16afed5
Luis Herdeth <luis.herdeth@gmail.com>
2023-01-31 17:52:38 +0100

    gifs v1.0

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:luisherdeth2020/test5 main:gh-pages
+# git push -f git@github.com:luisherdeth2020/gifsAnimados.git main:gh-pages
 
 cd -
